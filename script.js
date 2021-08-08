072272f42fa2738d388e1ce441e11d9ffa2c70a1
Dheeraj <djgupta820@gmail.com>
2021-08-08 07:09:35 +0530

    Resume created

---
+++ script.js
@@ -0,0 +1,35 @@
+function darkTheme()
+{
+    document.body.style.backgroundColor = "#1C1B1A";
+    document.body.style.color = "gray";
+    document.getElementById("obj").style.color = "#F1C40F";
+    document.getElementById("sa").style.color = "#F1C40F";
+    document.getElementById("name").style.color = "#F1C40F";
+    document.getElementById("pr").style.color = "#F1C40F";
+    document.getElementById("exp").style.color = "#F1C40F";
+    document.getElementById("ed").style.color = "#F1C40F";
+    document.getElementById("gss").style.color = "#F1C40F";
+    document.getElementById("btts").style.color = "#F1C40F";
+    document.getElementById("cl").style.color = "#F1C40F";
+    document.getElementById("os").style.color = "#F1C40F";
+    document.getElementById("cer").style.color = "#F1C40F";
+    document.getElementById("pp").style.color = "#F1C40F";
+}
+
+function lightTheme()
+{
+    document.body.style.backgroundColor = "white";
+    document.body.style.color = "black";
+    document.getElementById("obj").style.color = "brown";
+    document.getElementById("sa").style.color = "brown";
+    document.getElementById("name").style.color = "brown";
+    document.getElementById("pr").style.color = "brown";
+    document.getElementById("exp").style.color = "brown";
+    document.getElementById("ed").style.color = "brown";
+    document.getElementById("gss").style.color = "brown";
+    document.getElementById("btts").style.color = "brown";
+    document.getElementById("cl").style.color = "brown";
+    document.getElementById("os").style.color = "brown";
+    document.getElementById("cer").style.color = "brown";
+    document.getElementById("pp").style.color = "brown";
+}
